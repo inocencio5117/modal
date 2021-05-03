b99fac0bf6f5b7a2c4e4b32d9ae0c13d913234f3
Vinicius Inocêncio <vicnicius2009@gmail.com>
2021-05-02 21:18:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const modalClose = document.querySelector('.close-btn')
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('#overlay');
+
+document.addEventListener('click', e => {
+    const el = e.target;
+
+    if (el.classList.contains('open-m')) {
+        modal.classList.add('active');
+        overlay.classList.add('active');
+    }
+
+    if (el.classList.contains('close-btn')) {
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+})
+
+overlay.addEventListener('click', (e) => {
+    const el = e.target;
+
+    if (el.classList.contains('overlay')) {
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+})
+
+
+class FormValidation {
+    constructor() {
+        
+    }
+}
